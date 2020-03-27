c93145e95b0d5c7ea285e62a2e128b78ea965c98
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-03-28 00:17:07 +0430

    one to many polymorphic relation

--- app/Video.php
+++ app/Video.php
@@ -4,15 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Video extends Model
 {
-    protected $guarded = [];
-
-    public function image()
-    {
-        return $this->morphOne(Image::class, 'imageable');
-    }
-
     public function comments()
     {
         return $this->morphMany(Comment::class, 'commentable');
