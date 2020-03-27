f1416a3ce0155ddd767e0d702992a2740cd1a51c
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-03-27 13:41:43 +0430

    service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Billing\BankPaymentGateway;
+use App\Billing\CreditPaymentGateway;
+use App\Billing\PaymentGatewayContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // every time PaymentGateway class gets called the first instance that was invoked will be called
+        // but every time $this->app->bind() gets called a fresh instance will initialize
+        $this->app->singleton(PaymentGatewayContract::class, function ($app) {
+            if (request()->has('credit')) {
+                return new CreditPaymentGateway('usd');
+            }
+            return new BankPaymentGateway('usd');
+        });
+
+
     }
 
     /**
