3c30cf7763bf110789797ff70b5a02f44b4aaf88
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-07-18 12:40:51 +0430

    tested queues & jobs with Laravel Horizon

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+         Horizon::routeSlackNotificationsTo('https://hooks.slack.com/services/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', '#horizon');
+
+        // Horizon::night();
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewHorizon', function ($user) {
+            return in_array($user->email, [
+                'muhsenmaqsudi@gmail.com'
+            ]);
+        });
+    }
+}
